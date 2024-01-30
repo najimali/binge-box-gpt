4c2ebad53906afa5b4fd4629440eadc61d38e07c
najimali <mohd.najim@zyod.com>
2024-01-30 18:07:16 +0530

    Added Firebase Config

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApT2S_lvQy8DTYMG7Gk8_ayh4M93xI2-Y",
+  authDomain: "binge-box-b7f9c.firebaseapp.com",
+  projectId: "binge-box-b7f9c",
+  storageBucket: "binge-box-b7f9c.appspot.com",
+  messagingSenderId: "796624876813",
+  appId: "1:796624876813:web:c730dca042f7b63752ac44"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
